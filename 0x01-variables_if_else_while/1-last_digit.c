c19a553cec6b4fb05ab043ad9da54e4e106a056d
JoyNnenna <uzoegbojoy28@gmail.com>
2022-10-14 11:59:51 -0700

    My commit 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,11 +23,11 @@ if (n > 5)
 {
 	printf("%d and is greater than 5\n", n);
 }
-if (n == 0)
+else if (n == 0)
 {
 	printf("%d and is 0\n", n);
 }
-if (n < 6 != 0)
+else if ((n < 6) && (n != 0))
 {
 	printf("%d and is less than 6 and not 0\n", n);
 }
