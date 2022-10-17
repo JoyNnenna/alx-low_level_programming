735eae7f07273810d95902148d0c55f374226a37
JoyNnenna <uzoegbojoy28@gmail.com>
2022-10-17 11:56:36 -0700

    Commited task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * Description: A C program that prints all
+ * possible different combinations of three digits
+ * Return: (0) (Successful)
+*/
+
+int main(void)
+{
+	int m, i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (m = '0'; m <= '9'; m++)
+			{
+				if (i < j && j < m)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(m);
+					if (i == '7' && j == '8' && m == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
