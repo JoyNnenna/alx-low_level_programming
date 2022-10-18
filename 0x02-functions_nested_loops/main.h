1de55b759380137574d079fb3930f67da032ecd8
JoyNnenna <uzoegbojoy28@gmail.com>
2022-10-18 04:59:09 -0700

    My first commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File - main.h
- * Author: Andy Ajah
- * Description: Header contains all prototypes for all  
- * functions used in 0x02-functions_nested_loops directory
-*/
-
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -33,5 +23,3 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
-
-#endif
