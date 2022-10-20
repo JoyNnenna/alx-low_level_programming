228f0c36afe3ec412b9cd5bb60e4e375ba0c9869
JoyNnenna <uzoegbojoy28@gmail.com>
2022-10-20 13:57:45 -0700

    My commit 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * _isupper -> this is a function to check upper case alpas
  * @c: an integer paramer
  * Return: something but success
 */
-
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
