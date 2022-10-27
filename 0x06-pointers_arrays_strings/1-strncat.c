c6a41fc23b41247cbff2f591e020560703fc4533
JoyNnenna <uzoegbojoy28@gmail.com>
2022-10-27 07:59:32 -0700

    My commit for 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"                                                        
+#include <string.h>
+
+/**
+ * _strncat -> function to append some characters
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: string
+*/
+char strinet(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
